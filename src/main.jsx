eb0c55dbf63ee68b3137f4ccdc0e5a6e92d62886
shumilija <shumilija@ro.ru>
2024-08-06 22:32:37 +0700

    Задал базовую разметку страницы.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Template } from "./template.jsx";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
