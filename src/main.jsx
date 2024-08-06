5e0fe8983537c6546c144a5c0f66e3901fe1af54
shumilija <shumilija@ro.ru>
2024-08-06 21:16:39 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Template } from "./template.jsx";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Template />
+    </React.StrictMode>,
+)
---
+++ src/template.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class Template extends React.Component {
+    render() {
+
+        return (
+            <div/>
+        );
+    }
+}
